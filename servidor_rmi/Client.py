fb44258361df24420d151921b2b6798dd47503ff
RangersonTI <rangerson14@outlook.com>
2024-06-18 16:33:09 -0300

    .

--- servidor_rmi/Client.py
+++ servidor_rmi/Client.py
@@ -1,46 +1,7 @@
 import Pyro5.api as Pyro5
 
-ns = Pyro5.locate_ns()
-url = ns.lookup('obj')
+name_server = Pyro5.locate_ns()
+url = name_server.lookup('obj')
 o = Pyro5.Proxy(url)
 
 print(o.saludar())
-
-
-
-
-#def main():
-#    # Conecta ao serviço de arquivo no servidor Pyro
-#    file_service = Pyro5.api.Proxy("PYRO:Pyro.NameServer@localhost:9090")
-#
-#    # Lista arquivos no diretório especificado
-#    directory_path = "/Downloads/"
-#    try:
-#        files = file_service.list_files(directory_path)
-#        print("Files:", files)
-#    except Exception as e:
-#        print("Error listing files:", e)
-#
-#    # Lê o conteúdo de um arquivo especificado
-#    file_path = "/Downloads/"
-#    try:
-#        content = file_service.read_file(file_path)
-#        print("File Content:", content)
-#    except Exception as e:
-#        print("Error reading file:", e)
-#
-#    # Baixa um arquivo em partes
-#    try:
-#        download_path = "/some/directory/file.txt"
-#        file_size = file_service.get_file_size(download_path)
-#        chunk_size = 1024 * 1024  # 1 MB
-#        with open("downloaded_file.txt", "wb") as f:
-#            for start in range(0, file_size, chunk_size):
-#                data = file_service.download_file(download_path, start, chunk_size)
-#                f.write(data)
-#        print("File downloaded successfully!")
-#    except Exception as e:
-#        print("Error downloading file:", e)
-#
-#if __name__ == "__main__":
-#    main()
--- servidor_rmi/Server.py
+++ servidor_rmi/Server.py
@@ -4,6 +4,7 @@
 
 import Pyro5.api as Pyro5
 import Pyro5.errors as Pyro5_error
+
 from FileService import FileService
 
 @Pyro5.expose
@@ -14,46 +15,8 @@ class Saludo:
 daemon = Pyro5.Daemon()
 
 url = daemon.register(Saludo)
+name_server = Pyro5.Daemon()
+name_server.register('obj', url)
 
-try:
-    ns = Pyro5.locate_ns()
-    print("Name_server encontrado")
-except Pyro5_error.NamingError as e:
-    print("Erro ao encontrar o Name_Server: ",e)
-    exit(1)
-    
-try:
-    objetos = url.list()
-    print("Obj registrados: ")
-    for name, uri in objetos.items():
-        print(f"Nome: {name}, URI: {uri}")
-except Exception as e:
-    print("Erro ao listar objetos no Name Server:", e)
-
-#ns.register('obj', url)
 print(url)
-
 daemon.requestLoop()
-
-
-#def main():
-#    # Cria o objeto de serviço de arquivo
-#    file_service = FileService()
-#
-#    # Inicia o daemon Pyro 
-#    daemon = Pyro5.api.Daemon()
-#
-#    # Registra o objeto de serviço no daemon
-#    uri = daemon.register(file_service)
-#
-#    # Disponibiliza o serviço para o nome "FileService"
-#    ns = Pyro5.api.locate_ns(host="nameserver", port=3000)
-#    ns.register("serverrmi", uri)
-#
-#    print("FileService is running. URI:", uri)
-#
-#    # Mantém o servidor rodando
-#    daemon.requestLoop()
-#
-#if __name__ == "__main__":
-#    main()
