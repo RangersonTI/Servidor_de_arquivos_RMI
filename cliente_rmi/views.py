eb6294e708b923ee3287539b2b24fc0d65983624
RangersonTI <rangerson14@outlook.com>
2024-06-21 23:37:24 -0300

    implementado a interface gráfica enviar arquivo e logica

--- cliente_rmi/views.py
+++ cliente_rmi/views.py
@@ -4,7 +4,7 @@ from .models import Enviar_Arquivo
 
 # Create your views here.
 
-URI_PYRO5 = "PYRO:obj_ae2eb3ecf13445dfa8934663a0eaaeeb@localhost:3000"
+URI_PYRO5 = "PYRO:obj_64c224b013c04337a7bdabdc0270322f@localhost:3000"
 
 uri_server = Pyro5.Proxy(URI_PYRO5)
 
@@ -18,17 +18,22 @@ def visualizar_arquivos(request):
     return render(request, 'visualizar.html', context)
 
 def enviar_arquivos(request):
+    
+    print("Formulário enviado via POST")
+    print("Conteúdo de FILES:", request.FILES)
+    print("Conteúdo de POST:", request.POST)
+    
     if request.method == 'POST' and request.FILES.get('file'):
         arquivo = request.FILES['file']
-        nome_arquivo = request.POST.get('nome_arquivo', '')
-        url_arquivo = request.POST.get('url_arquivo', '')
+        nome_arquivo = request.POST.get('nome_arquivo')
+        url_arquivo = request.POST.get('url_arquivo')
 
-        if nome_arquivo and url_arquivo:
+        if nome_arquivo and arquivo:
             try:
                 # Salvando o arquivo no banco de dados (ou fazendo outra operação necessária)
-                novo_arquivo = Enviar_Arquivo(nome_arquivo=nome_arquivo, url_arquivo=url_arquivo, arquivo=arquivo)
+                novo_arquivo = Enviar_Arquivo(nome_arquivo=nome_arquivo, arquivo=arquivo)
                 novo_arquivo.save()
-
+                print("Arquivo salvo com sucesso")
                 # Redireciona de volta para a página de visualização de arquivos
                 return redirect('visualizar_arquivos')
             except Exception as ex:
