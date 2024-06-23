6efcb4331618437c62ef08c04bf8eecc3a43f2be
RangersonTI <rangerson14@outlook.com>
2024-06-23 09:00:09 -0300

    Delete urls.py

--- cliente_rmi/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.contrib import admin
-from .views import Visualizar_arquivos, Enviar_arquivos, Deletar_arquivo, Download_arquivo
-
-urlpatterns = [
-    
-]
