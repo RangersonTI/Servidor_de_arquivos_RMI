eb2fa2a36f1efbbf8b7471b3c160d63aab536990
Rangerson Clemente <rangerson14@outlook.com>
2024-06-23 11:04:26 -0300

    Revert "Delete urls.py"
    This reverts commit 6efcb4331618437c62ef08c04bf8eecc3a43f2be.

---
+++ cliente_rmi/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from django.contrib import admin
+from .views import Visualizar_arquivos, Enviar_arquivos, Deletar_arquivo, Download_arquivo
+
+urlpatterns = [
+    
+]
